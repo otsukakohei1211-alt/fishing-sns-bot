9993bbc8d3776b158bcf1849b36028dd7091e7d2
koheiotsuka <koheiotsuka@koheiotsukanoMac-mini.local>
2026-06-05 03:20:04 +0900

    Include Noto Sans JP font in OGP serverless bundle

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/og": [
+      "./node_modules/@fontsource/noto-sans-jp/files/noto-sans-jp-japanese-700-normal.woff2",
+      "./node_modules/@fontsource/noto-sans-jp/files/noto-sans-jp-latin-700-normal.woff2",
+    ],
+  },
 };
 
 export default nextConfig;
